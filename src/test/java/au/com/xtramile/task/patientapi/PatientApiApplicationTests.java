3523d145f2ef0cf27471a8c29139e5aab631fa8a
arie kiswanto <arie.kiswanto12@gmail.com>
2021-09-24 06:43:32 +0700

    baseline

---
+++ src/test/java/au/com/xtramile/task/patientapi/PatientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.xtramile.task.patientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
